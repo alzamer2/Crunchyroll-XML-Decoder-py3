690eb37d99eafc7220c6816b68069a8f859df0e8
alzamer2 <alzamer2@gmail.com>
2019-03-30 16:53:12 +0300

    Add files via upload

---
+++ crunchy-xml-decoder-py3.bat
@@ -0,0 +1,10 @@
+@echo off
+set PYTHONHTTPSVERIFY=0 
+RD "%PUBLIC%\Crunchyroll-XML-Decoder_link" 1>NUL 2>NUL
+mklink /j "%PUBLIC%\Crunchyroll-XML-Decoder_link" %cd% 1>NUL 2>NUL
+cd "%PUBLIC%\Crunchyroll-XML-Decoder_link"
+:sratre
+crunchy-xml-decoder-py3.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+RD "%PUBLIC%\Crunchyroll-XML-Decoder_link" 1>NUL 2>NUL
+pause
+
